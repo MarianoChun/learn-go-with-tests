ce085e8d240cb281c1844971cf34e8cbdaabf576
Mariano Chun <marianochun01@gmail.com>
2024-01-05 23:34:49 -0300

    Refactor Repeat function to use standard library function

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,10 +1,8 @@
 package iteration
 
+import "strings"
+
 // Repeat takes a character and a repeat count and return a string with that same character repeated the specified number of repetitions
 func Repeat(character string, repeatCount int) string {
-	var result string
-	for i := 0; i < repeatCount; i++ {
-		result += character
-	}
-	return result
+	return strings.Repeat(character, repeatCount)
 }
